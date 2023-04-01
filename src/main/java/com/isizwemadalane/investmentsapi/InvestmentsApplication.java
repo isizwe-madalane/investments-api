4c1b15a075d7f0c73806dfbbc878c9a622492b6b
isizwe-madalane <isizwe.madalane@gmail.com>
2023-04-01 12:27:57 +0200

    Init Commit

---
+++ src/main/java/com/isizwemadalane/investmentsapi/InvestmentsApplication.java
@@ -0,0 +1,13 @@
+package com.isizwemadalane.investmentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentsApplication.class, args);
+	}
+
+}
